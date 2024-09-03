4c2a73b16fbf51c6adab4892ba00b7802982fcfb
Vadim Zadorskii <snbkrise@gmail.com>
2024-09-03 15:01:33 +0200

    feat: Add configuration options to PDF parsing function

--- aider/llama_parse.py
+++ aider/llama_parse.py
@@ -1,6 +1,13 @@
 from llama_parse import LlamaParse
 
+
 def parse_pdf(pdf_file, api_key):
-    llama = LlamaParse(api_key=api_key)
-    result = llama.parse_file(pdf_file)
-    return result.text
+    parser = LlamaParse(
+        api_key=api_key,  # can also be set in your env as LLAMA_CLOUD_API_KEY
+        result_type="markdown",  # "markdown" and "text" are available
+        num_workers=4,  # if multiple files passed, split in `num_workers` API calls
+        verbose=True,
+        language="en",  # Optionally you can define a language, default=en
+    )
+    documents = parser.load_data(pdf_file)
+    return documents[0].text
